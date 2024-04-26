78b85851df72f5f414926007e6562eaa8e553a01
DEEPAK KUMAR <deepak.kumar@gohashinclude.com>
2024-04-26 15:37:14 +0530

    remove some code.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.IntegerField()
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
