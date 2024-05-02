8fa0e7ee5adebdfdfbf60498de22ed801a2c4dbd
DEEPAK KUMAR <deepak.kumar@gohashinclude.com>
2024-05-02 14:09:46 +0530

    autocomplete and horizontal search added in All FK

--- blog/admin.py
+++ blog/admin.py
@@ -13,25 +13,26 @@ class UserAdmin(ExportActionMixin, OrigUserAdmin):
     list_filter = ('id', 'username', 'email')
     search_fields = ('id', 'username', 'email')
 
+
 @admin.register(Comment)
 class CommentAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'body', 'post', 'created', 'updated', 'active', 'parent')
     list_filter = ('id', 'email', 'active')
     search_fields = ('id', 'email', 'active',)
-
+    autocomplete_fields = ['post','parent']
 
 @admin.register(Post)
 class PostAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ('id', 'author', 'category', 'title', 'text', 'created_date', 'published_date', 'feature_img','thumbnail_img', 'slug')
     list_filter = ('id', 'author', 'category')
     search_fields = ('id', 'slug')    
-
+    autocomplete_fields = ['author','category']
+    
 @admin.register(Category)
 class CategoryAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ('id', 'name', 'desc', 'slug')
     list_filter = ('id', 'slug')
     search_fields = ('id', 'slug')
-
 
 @admin.register(Tag)
 class TagAdmin(ExportActionMixin, admin.ModelAdmin):
