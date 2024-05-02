990f37ba0aa0e9a6152313cdef293551f41fa51b
DEEPAK KUMAR <deepak.kumar@gohashinclude.com>
2024-05-02 12:04:49 +0530

    search and list filter added in admin.

--- blog/admin.py
+++ blog/admin.py
@@ -9,27 +9,34 @@ User = get_user_model()
 
 @admin.register(User)
 class UserAdmin(ExportActionMixin, OrigUserAdmin):
-  list_display = (
-    'id', 'first_name', 'last_name', 'username', 'email','age','mobile'
-  )
+    list_display = ('id', 'first_name', 'last_name', 'username', 'email','age','mobile')
+    list_filter = ('id', 'username', 'email')
+    search_fields = ('id', 'username', 'email')
 
 @admin.register(Comment)
 class CommentAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created', 'active')
-    list_filter = ('active', 'created', 'updated')
-    search_fields = ('name', 'email', 'body')
+    list_display = ('id', 'name', 'email', 'body', 'post', 'created', 'updated', 'active', 'parent')
+    list_filter = ('id', 'email', 'active')
+    search_fields = ('id', 'email', 'active',)
 
 
 @admin.register(Post)
 class PostAdmin(ExportActionMixin, admin.ModelAdmin):
-    pass
+    list_display = ('id', 'author', 'category', 'title', 'text', 'created_date', 'published_date', 'feature_img','thumbnail_img', 'slug')
+    list_filter = ('id', 'author', 'category')
+    search_fields = ('id', 'slug')    
 
 @admin.register(Category)
 class CategoryAdmin(ExportActionMixin, admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'desc', 'slug')
+    list_filter = ('id', 'slug')
+    search_fields = ('id', 'slug')
+
 
 @admin.register(Tag)
 class TagAdmin(ExportActionMixin, admin.ModelAdmin):
-    pass
+    list_display = ('id', 'slug')
+    list_filter = ('id', 'slug')
+    search_fields = ('id', 'slug')
 
 
